2f079765bf7b4d48fd7679de669c97ef5d3e0b1d
Bastian <bvasquez2025@udec.cl>
2026-05-29 14:05:28 -0400

    Crear clase Deposito

---
+++ Proyecto_Tarea3/src/main/java/Expendedor/Deposito.java
@@ -0,0 +1,50 @@
+package Expendedor;
+import java.util.ArrayList;
+/**
+ * Clase que representa un depósito genérico.
+ *
+ * @param <T> tipo de objeto que guarda el depósito
+ */
+public class Deposito<T>{
+
+	/**
+	 * Creamos el array genérico
+	 */
+
+	private ArrayList<T> almacen; //almacen es de tipo generico
+
+	/**
+	 * Constructor del arreglo
+	 */
+
+	public Deposito(){
+		almacen = new ArrayList<>();
+	}
+
+	/**
+	 * Método para añadir una vez un objeto al arreglo
+	 *
+	 * @param objeto objeto que se quiere añadir al arreglo
+	 */
+
+	public void addObjeto(T objeto){ //Obtenemos un objeto generico
+		almacen.add(objeto); //Se agrega el objeto generico, ya sea Moneda, Bebida o Dulce		
+	}
+
+	/**
+	 * Método para retirar el objeto
+	 *
+	 * @return objeto retirado del depósito o null si está vacío
+	 */
+
+	public T getObjeto() { //Retorna una Moneda o Bebida o Dulce
+        if (almacen.isEmpty()) { //Si el almacen no tiene objetos, retornamos null
+            return null;
+        }
+        return almacen.remove(0); //Si tiene un objeto, removemos el objeto del almacen y lo retornamos
+    }
+
+	public boolean isEmpty(){ //Para que otros metodos puedan ver si esta vacio
+		return almacen.isEmpty();
+	}
+}
