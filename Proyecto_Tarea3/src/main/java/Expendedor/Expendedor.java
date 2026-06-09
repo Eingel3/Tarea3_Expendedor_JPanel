0d6d3859793d77991dba4a2c0a1c60e2d4219745
Bastian <bvasquez2025@udec.cl>
2026-06-09 17:57:25 -0400

    Fix: Arreglar error que no rellenaba los depositos en el metodo llenarDepositosVacios en Expendedor con la cantidad de objetos correcta

--- Proyecto_Tarea3/src/main/java/Expendedor/Expendedor.java
+++ Proyecto_Tarea3/src/main/java/Expendedor/Expendedor.java
@@ -64,23 +64,27 @@ public class Expendedor {
      * @param cantProducto es la cantidad de productos con las que se desean rellenar los depositos vacios
      */
     public void llenarDepositosVacios(int cantProducto){
-        for(int i = 0; i < cantProducto; i++){
-            if coca.isEmpty() {
+            if (coca.isEmpty()) {
+            for(int i = 0; i < cantProducto; i++){
                 coca.addObjeto(new CocaCola(InformacionProducto.COCACOLA.getTipo() * 100 + i));
-            }
+            }}
             if (fanta.isEmpty()){
-                fanta.addObjeto(new Fanta(InformacionProducto.FANTA.getTipo()*100 + i));
-            }
+                for(int i = 0; i < cantProducto; i++) {
+                    fanta.addObjeto(new Fanta(InformacionProducto.FANTA.getTipo() * 100 + i));
+                }}
             if (super8.isEmpty()){
-                super8.addObjeto(new Super8(InformacionProducto.SUPER8.getTipo()*100 + i));
-            }
+                for(int i = 0; i < cantProducto; i++) {
+                    super8.addObjeto(new Super8(InformacionProducto.SUPER8.getTipo() * 100 + i));
+                }}
             if (sprite.isEmpty()){
-                sprite.addObjeto(new Sprite(InformacionProducto.SPRITE.getTipo()*100 + i));
-            }
+                for(int i = 0; i < cantProducto; i++) {
+                    sprite.addObjeto(new Sprite(InformacionProducto.SPRITE.getTipo() * 100 + i));
+                }}
             if (snicker.isEmpty()){
-                snicker.addObjeto(new Snicker(InformacionProducto.SNICKER.getTipo()*100 + i));
-            }
-        }
+                for(int i = 0; i < cantProducto; i++) {
+                    snicker.addObjeto(new Snicker(InformacionProducto.SNICKER.getTipo() * 100 + i));
+                }}
+
     }
 
     /**
