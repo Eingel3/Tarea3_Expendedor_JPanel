42a7aecca35f1eb994705ddfa6aec3efad7639dd
Bastian <bvasquez2025@udec.cl>
2026-06-09 20:40:41 -0400

    Feat: Cambiar los precios de los productos en InformacionProducto para tener una variedad de precios mas alta

--- Proyecto_Tarea3/src/main/java/Productos/InformacionProducto.java
+++ Proyecto_Tarea3/src/main/java/Productos/InformacionProducto.java
@@ -5,10 +5,10 @@ package Productos;
  */
 
 public enum InformacionProducto {
-	SPRITE(500, 11), //SPRITE llama a su constructor mediante 500
-	COCACOLA(500, 12), //los tipos de las bebidas empiezan por 1, los dulces por 2
-	SNICKER(500, 21), //el segundo numero diferencia el tip de bebida o dulce
-	SUPER8(500, 22),
+	SPRITE(549, 11), //SPRITE llama a su constructor mediante 500
+	COCACOLA(599, 12), //los tipos de las bebidas empiezan por 1, los dulces por 2
+	SNICKER(199, 21), //el segundo numero diferencia el tip de bebida o dulce
+	SUPER8(270, 22),
 	FANTA(500, 13);
 	private final int precio; //Todos los precios han de ser multiplos de 100, ya que todas las monedas son multiplo de 100
 	private final int tipo;
